a9b492cf0f65c16ea38a0c35371573663c19915c
Ali mirzajanzadeh <ali.mjz93@gmail.com>
2024-09-11 01:23:57 +0330

    initial commit

--- src/main/java/org/dynamic/proxies/DynamicProxyDemo.java
+++ src/main/java/org/dynamic/proxies/DynamicProxyDemo.java
@@ -4,7 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 
 public class DynamicProxyDemo {
-    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) {
         MyInterface realObject = new MyInterfaceImpl();
         MyInterface proxyObject = (MyInterface) Proxy.newProxyInstance(MyInterface.class.getClassLoader(),
                 new Class[]{MyInterface.class}, new TestInvocationHandler(realObject));
